b2c7ff7beeabca6427f9b54f066ccca414851dfc
nimr77 <nimr.h.sawafta@gmail.com>
2023-12-13 14:14:36 +0100

    update elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      profile:{
-        
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(139, 2, 133, 255)",
+          "0 0px 65px rgba(139, 2, 133, 255)",
+        ],
       },
-      colors:{
+      profile: {},
+      colors: {
         "bg-1-color": "#1B1B1B",
         "nav-color": "#171717",
         "nav-glow": "#8B0285",
-      }
+      },
     },
-        fontFamily: {
-
-              'fontFamily': ['Inter',]
-
+    fontFamily: {
+      fontFamily: ["Inter"],
     },
   },
   plugins: [],
