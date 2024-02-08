eaa2e2790a6c512eac93967cf70bc1e85040329c
nimr77 <nimr.h.sawafta@gmail.com>
2024-02-08 23:20:53 +0100

    update stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nimr77.github.io",
-  base: "/my-portofolio/",
+  base: "/my-portfolio/",
   integrations: [tailwind(), react()],
 });
